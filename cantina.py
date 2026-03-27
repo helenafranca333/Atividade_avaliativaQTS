ef60ca08e5ac0cca5e37bbfa8be531d122bb3a7c
Maria Helena <franca.helena333@gmail.com>
2026-03-27 17:10:25 -0300

    trabalho branch finalizado

--- cantina.py
+++ cantina.py
@@ -1,4 +1,8 @@
 def calcular_total(quantidade, valor_unitario):
     return quantidade * valor_unitario
 
+def validar_pedido(item, quantidade, valor_unitario):
+    if not item or quantidade <= 0 or valor_unitario <= 0:
+        return "Pedido inválido"
+    return "Pedido válido"
 
